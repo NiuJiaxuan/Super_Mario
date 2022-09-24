21cc6816d180db29356e33bf11300e1e4d8e0b40
Ray Wei <wei.1104@buckeyemail.osu.edu>
2022-09-23 23:20:32 -0400

    temp

---
+++ MyFirstGame/Sprites/SpriteFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Sprites
+{
+    abstract class SpriteFactory 
+    {
+        protected Random random;
+
+        protected SpriteFactory()
+        {
+            random = new Random(DateTime.Now.Millisecond);
+        }
+        public abstract Sprite Create();
+    }
+
+    class PlayerOneAvatar : SpriteFactory
+    {
+        private static SpriteFactory instance;
+
+        private static SpriteFactory Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new PlayerOneAvatar();
+                }
+                return instance;
+            }
+        }
+
+        public override Sprite Create()
+        {
+            return new MarioSprite();
+        }
+    }
+
+
+
+}
